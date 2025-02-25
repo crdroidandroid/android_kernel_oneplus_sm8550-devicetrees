e6f06150613af7504e9fee9e17908b24efa23309
Bruno Martins <bgcngm@gmail.com>
2025-02-25 21:46:55 +0000

    camera-devicetree: Import from OOS 15.0.0.401(EX01)
    Change-Id: I44477d6c51d2f4eeea918db1b961b438775afc20

--- qcom/camera/config/kalama.mk
+++ qcom/camera/config/kalama.mk
@@ -1,11 +1,24 @@
 dtbo-$(CONFIG_ARCH_KALAMA) := kalama-camera.dtbo
-dtbo-$(CONFIG_ARCH_KALAMA) += kalama-camera-sensor-cdp.dtbo \
-								kalama-camera-sensor-mtp.dtbo \
-								kalama-camera-sensor-qrd.dtbo \
-								kalama-camera-sensor-hdk.dtbo \
-								kalama-sg-hhg-camera.dtbo \
-								kalama-sg-hhg-camera-sensor.dtbo
+#dtbo-$(CONFIG_ARCH_KALAMA) += kalama-camera-sensor-cdp.dtbo \
+#								kalama-camera-sensor-mtp.dtbo \
+#								kalama-camera-sensor-qrd.dtbo \
+#								kalama-camera-sensor-hdk.dtbo \
+#								kalama-sg-hhg-camera.dtbo \
+#								kalama-sg-hhg-camera-sensor.dtbo
 
-dtbo-$(CONFIG_ARCH_CROW) += crow-camera.dtbo \
-				crow-camera-sensor-idp.dtbo \
-				crow-camera-sensor-qrd.dtbo
+#OPLUS_DTS_OVERLAY start
+dtbo-$(CONFIG_ARCH_KALAMA) += oplus/salami-camera-overlay.dtbo \
+dtbo-$(CONFIG_ARCH_KALAMA) += oplus/xueying-camera-overlay.dtbo \
+dtbo-$(CONFIG_ARCH_KALAMA) += oplus/xigua-camera-overlay.dtbo  \
+dtbo-$(CONFIG_ARCH_KALAMA) += oplus/aston-camera-overlay.dtbo  \
+dtbo-$(CONFIG_ARCH_KALAMA) += oplus/aston-camera-overlay-T0.dtbo  \
+dtbo-$(CONFIG_ARCH_KALAMA) += oplus/astondomastic-camera-overlay-T0.dtbo  \
+#OPLUS_DTS_OVERLAY end
+
+dtbo-$(CONFIG_ARCH_CROW) += crow-camera.dtbo
+# 				crow-camera-sensor-idp.dtbo \
+# 				crow-camera-sensor-qrd.dtbo
+
+#OPLUS_DTS_OVERLAY start
+dtbo-$(CONFIG_ARCH_CROW) += oplus/benz-camera-overlay.dtbo
+#OPLUS_DTS_OVERLAY end
